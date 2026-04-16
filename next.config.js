761eea2674715da8b35dfdb4f232b7bbca7724d7
Abin.Thampi <se014801@taqa-ws.com>
2026-04-16 10:32:50 +0400

    Initial commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+
 const nextConfig = {
   reactStrictMode: true,
+  ...(isProd && {
+    output: 'export',
+    basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
+    assetPrefix: process.env.NEXT_PUBLIC_BASE_PATH || '',
+  }),
 }
 
 module.exports = nextConfig
